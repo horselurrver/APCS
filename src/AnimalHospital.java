e94b4a2138117d965e3ed2dcc9483a1e12af9073
Amy Wang <horselurrver@gmail.com>
2017-03-02 22:12:47 -0600

    Added barebones structure for AnimalHospital

---
+++ src/AnimalHospital.java
@@ -0,0 +1,40 @@
+/**
+ * Read in boarding pets from a file and execute different methods
+ *
+ */
+public class AnimalHospital {
+	public AnimalHospital(String inputFile){
+		
+	}
+	
+	/**
+	 * Searches the list of pets for every pet of given name
+	 * and prints information using toString() method
+	 * @param name
+	 */
+	public void printPetInfoByName(String name){
+		
+	}
+	
+	/**
+	 * Searches the list of pets for pets owned by given
+	 * person and prints the information using toString() 
+	 * method
+	 * @param name
+	 */
+	public void printPetInfoByOwner(String name){
+		
+	}
+	
+	/**
+	 * Searches the list of pets for every pet boarding 
+	 * at the given time and prints the information using
+	 * toString()
+	 * @param month
+	 * @param day
+	 * @param year
+	 */
+	public void printPetsBoarding(int month, int day, int year){
+		
+	}
+}
