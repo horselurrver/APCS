492afa7422bc2d27b5b2ba39190a35ca94d838c9
Amy Wang <horselurrver@gmail.com>
2017-03-02 21:50:44 -0600

    Added signatures to interface

--- src/Boardable.java
+++ src/Boardable.java
@@ -1,4 +1,6 @@
 
-public class Boardable {
-
+public interface Boardable {
+	public void setBoardStart(int month, int day, int year);
+	public void setBoardEnd(int month, int day, int year);
+	public boolean boarding(int month, int day, int year);
 }
