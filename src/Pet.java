171570223dad550911f601eee227137a7261d169
Amy Wang <horselurrver@gmail.com>
2017-03-02 22:23:10 -0600

    Added barebones structure for Pet

---
+++ src/Pet.java
@@ -0,0 +1,70 @@
+/**
+ * Blueprint for residential animals
+ *
+ */
+public class Pet {
+	private String name;
+	private String ownerName;
+	private String color;
+	private String ownerEmail;
+	protected int gender;
+	
+	public Pet(String name, String ownerName, String ownerEmail, String color){
+		
+	}
+	
+	/**
+	 * Returns pet name
+	 * @return
+	 */
+	public String getPetName(){
+		return name;
+	}
+	
+	/**
+	 * Returns owner name
+	 * @return
+	 */
+	public String getOwnerName(){
+		return ownerName;
+	}
+	
+	/**
+	 * Returns color of pet
+	 * @return
+	 */
+	public String getColor(){
+		return color;
+	}
+	
+	/**
+	 * Sets the gender of the pet
+	 * @param genderId
+	 */
+	public void setGender(int genderId){
+		gender = genderId;
+	}
+	
+	/**
+	 * Need to fix this method, not sure of what id maps to what gender
+	 * @return
+	 */
+	public String getGender(){
+		return "";
+	}
+	
+	/**
+	 * Returns owner email
+	 * @return
+	 */
+	public String getEmail(){
+		return ownerEmail;
+	}
+	
+	/**
+	 * Need to fix this method, not sure what information this is supposed to return
+	 */
+	public String toString(){
+		return "";
+	}
+}
