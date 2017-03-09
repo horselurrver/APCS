90007ffc40c5931ea5719d96d4564d2befa7482b
sjakka27 <spoorthijakka@gmail.com>
2017-03-09 15:51:42 -0600

    Update PetTester.java
    Added printout of information on each Pet

--- src/PetTester.java
+++ src/PetTester.java
@@ -7,6 +7,29 @@ public class PetTester {
 	public PetTester(){
 		try {
 			AnimalHospital h = new AnimalHospital("petData.txt");//file not found?
+			
+			for(Pet p: h)
+			{
+				
+				System.out.println("******************************************");
+				if p instanceof Cat
+				{
+					System.out.println("CAT: ");
+				}
+				else if p instanceof Dog
+				{
+					System.out.println("DOG: ");
+					
+				}
+				else if p instanceof Bird
+				{
+					System.out.println("BIRD");
+				}
+				
+				System.out.println(p.toString);
+				System.out.println("******************************************");
+				
+			}
 		} catch (IllegalEmailException i){
 			System.out.println(i.getMessage());
 		}
